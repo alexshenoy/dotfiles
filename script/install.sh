c456fe4305ae7fa58d2b625b329b8a75944f60a5
Alex Shenoy <ashenoy@4moms.com>
2016-09-14 20:41:26 -0600

    Fix install lint errors

--- script/install.sh
+++ script/install.sh
@@ -9,11 +9,14 @@ EXCLUDES="completion|script|brew"
 install_dotfiles () {
   echo 'installing dotfiles'
 
-  for src in $(ls -1 | egrep -v $EXCLUDES)
+  dotfiles=$(find . -maxdepth 1 -name "*" -print | sed "s/.\///" | grep "^\w" |
+            egrep -v $EXCLUDES)
+
+  for src in $dotfiles
   do
     target="$DOTFILES_ROOT/$src"
     dst="$HOME/.$src"
-    ln -sFfn $target $dst
+    ln -sFfn "$target" "$dst"
     echo "ln -sFfn $target $dst"
   done
 }
@@ -21,10 +24,9 @@ install_dotfiles () {
 install_dotfiles
 
 # Install Vundle Plugins
-if [ ! -d $DOTFILES_ROOT/vim/bundle ]; then
-    mkdir -p $DOTFILES_ROOT/vim/bundle
+if [ ! -d "$DOTFILES_ROOT/vim/bundle" ]; then
+    mkdir -p "$DOTFILES_ROOT/vim/bundle"
 fi
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
-
